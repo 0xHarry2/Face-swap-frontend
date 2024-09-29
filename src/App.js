47e62cdb75d478818838a5959460de317b9d22c8
0xHarry2 <0xharry517@gmail.com>
2024-09-29 07:37:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./styles/globals.css";
 
-const api = "https://magical-pigeon-annually.ngrok-free.app/"
+const api = "https://ebed-188-43-136-44.ngrok-free.app"
 
 const App = () => {
   const [imgSrc, setimgSrc] = useState("")
